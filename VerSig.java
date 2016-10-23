3ca2c4f9ba854be1367588531c0243ba68fc009f
ShingJan <yuanj.shi@hotmail.com>
2016-10-24 00:24:37 +0800

    mod function finished?

--- VerSig.java
+++ VerSig.java
@@ -2,30 +2,29 @@ import java.io.*;
 import java.security.*;
 import java.security.spec.*;
 
-class VerSig {
+public class VerSig {
 
-    public static void main() {
+    public static void main(String[] args) {
 
         /* Verify a DSA signature */
         int ver = 3;
         if (ver == 3) {
-            System.out.println(mod(5,5963));
-        } catch (Exception e) {
-            System.err.println("Caught exception " + e.toString());
+            VerSig result = new VerSig();
+            System.out.println(result.mod(5,5963));
         }
     }
 
     public int mod(int a, int b){
         //a is the public, b is the mod
-        if(!a || !b){
-                return;
-        }
+        /*if(a == null || b == null){
+                return null;
+        }*/
         int b1 = b + 1;
-        while(b%a != 0){
+        while(b1%a != 0){
             b1 += b;
         }
-
-        result result = b1/a;
+        int result = b1/a;
+        return result;
     }
 
 }
