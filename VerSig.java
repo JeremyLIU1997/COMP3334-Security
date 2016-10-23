75c7dae13e73b6bf335d9dac2cd629032c9ced31
ShingJan <yuanj.shi@hotmail.com>
2016-10-24 00:11:47 +0800

    mod function Update

--- VerSig.java
+++ VerSig.java
@@ -4,21 +4,28 @@ import java.security.spec.*;
 
 class VerSig {
 
-    public static void main(String[] args) {
+    public static void main() {
 
         /* Verify a DSA signature */
-
-        if (args.length != 3) {
-            System.out.println("Usage: VerSig " +
-                "publickeyfile signaturefile " + "datafile");
-        }
-        else try {
-
-        // the rest of the code goes here
-
+        int ver = 3;
+        if (ver == 3) {
+            System.out.println(mod(5,5963));
         } catch (Exception e) {
             System.err.println("Caught exception " + e.toString());
         }
+    }
+
+    public int mod(int a, int b){
+        //a is the public, b is the mod
+        if(!a || !b){
+                return;
+        }
+        int b1 = b + 1;
+        while(b%a != 0){
+            b1 += b;
+        }
+
+        result result = b1/a;
     }
 
 }
