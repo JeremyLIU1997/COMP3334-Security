b825c07b4bb83fb559e8e89a3e1e4f8f62169a29
ShingJan <yuanj.shi@hotmail.com>
2016-10-24 01:36:27 +0800

    dunno if I got the border case right

--- VerSig.java
+++ VerSig.java
@@ -10,15 +10,15 @@ public class VerSig {
         int ver = 3;
         if (ver == 3) {
             VerSig result = new VerSig();
-            System.out.println(result.mod(5,5963));
+            System.out.println(result.mod(35,78));
         }
     }
 
     public int mod(int a, int b){
         //a is the public, b is the mod
-        /*if(a == null || b == null){
-                return null;
-        }*/
+        if(a == 0 || b == 0){
+                return 0;
+        }
         int b1 = b + 1;
         while(b1%a != 0){
             b1 += b;
